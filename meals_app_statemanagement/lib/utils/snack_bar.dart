eef184dc9396d8b0dc100f3bcc0237eb4b4a5bff
Amanat Singh <amanatsinghnain@gmail.com>
2023-07-01 20:21:41 +0530

    completed state management

--- meals_app_statemanagement/lib/utils/snack_bar.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-void showSnackBar(BuildContext context, String message) {
-  ScaffoldMessenger.of(context).clearSnackBars();
-  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message)));
-}
